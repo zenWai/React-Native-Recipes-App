f1ee6d2c7689e996b2233d01a45e1a47aae362eb
Fernando <ivopresa@gmail.com>
2023-06-24 18:40:47 +0200

    NavigationContainer, Stack imp

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     /* StatusBar use - background app color is set on app.json*/
     return (
         <>
-            <StatusBar style={"light"}/>
-            <CategoriesScreen/>
+            <StatusBar style="dark"/>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Categories" component={CategoriesScreen}/>
+                    <Stack.Screen name={"MealsOverview"} component={MealsOverviewScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
         </>
     );
 }
