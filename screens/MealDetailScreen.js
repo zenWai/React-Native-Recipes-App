bbcb7865ffe6aa116373822d51f2d7ca21e3c4e8
Fernando <ivopresa@gmail.com>
2023-06-25 19:11:53 +0200

    Imp on header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,13 +1,29 @@
-import {Image, StyleSheet, Text, View, ScrollView} from "react-native";
+import {Image, StyleSheet, Text, View, ScrollView, Button} from "react-native";
 import {MEALS} from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
+import {useLayoutEffect} from "react";
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
     const mealId = route.params.mealId;
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+    function headerButtonPressHandler() {
+        console.log('Pressed');
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () =>{
+                return <Button
+                    title={'Tap me'}
+                    onPress={headerButtonPressHandler}
+                />
+            }
+        })
+    }, [navigation, headerButtonPressHandler]);
 
     return (
         <ScrollView style={styles.rootContainer}>
