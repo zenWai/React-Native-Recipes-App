76ae85634716984df893b043f9bff0b939dfcf5b
Fernando <ivopresa@gmail.com>
2023-06-25 17:28:37 +0200

    MealDetailScreen, Reusable Meal Details component

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,31 @@
-import {Text} from "react-native";
+import {Image, Text, View} from "react-native";
+import {MEALS} from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
 
 function MealDetailScreen({ route }) {
     const mealId = route.params.mealId;
+
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
     return (
-        <Text>Meal detail screen {mealId}</Text>
-    )
+        <View>
+            <Image source={{ uri: selectedMeal.imageUrl }}/>
+            <Text>{selectedMeal.title}</Text>
+            <MealDetails
+                duration={selectedMeal.duration}
+                affordability={selectedMeal.affordability}
+                complexity={selectedMeal.complexity}
+            />
+            <Text>Ingredients</Text>
+            {selectedMeal.ingredients.map(ingredient => (
+                <Text key={ingredient}>{ingredient}</Text>
+            ))}
+            <Text>Steps</Text>
+            {selectedMeal.steps.map((step) =>(
+                <Text key={step}>{step}</Text>
+            ))}
+        </View>
+    );
 }
 
 export default MealDetailScreen;
