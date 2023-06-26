77ad2f71cf680c3681cb744261969ae9884a72ad
Fernando <ivopresa@gmail.com>
2023-06-26 15:49:58 +0200

    Reusable MealsList, FavoritesScreen done. Using Context API

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,39 @@
-import {Text} from "react-native";
+import MealsList from "../components/MealsList/MealsList";
+import {FavoritesContext} from "../store/context/favorites-context";
+import {useContext} from "react";
+import {MEALS} from "../data/dummy-data";
+import {StyleSheet, Text, View} from "react-native";
 
 function FavoritesScreen() {
-    return <Text>FavoritesScreen</Text>
+    const favoriteMealsCtx = useContext(FavoritesContext);
+    const favoriteMeals =
+        MEALS.filter(meal =>
+            favoriteMealsCtx.ids.includes(meal.id)
+        );
+
+    if (favoriteMeals.length === 0) {
+        return (
+            <View style={styles.rootContainer}>
+                <Text style={styles.text}>
+                    No favorite meals found. Start adding some!
+                </Text>
+            </View>
+        );
+    }
+
+    return <MealsList displayMeals={favoriteMeals}/>
 }
 
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
 export default FavoritesScreen;
